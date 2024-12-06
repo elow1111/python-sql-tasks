31b0d8c50eed33172792a840ede7a59f39cdb509
Максим <160880502+elow1111@users.noreply.github.com>
2024-12-06 16:59:01 +0300

    solution.py

--- 4/src/solution.py
+++ 4/src/solution.py
@@ -6,5 +6,19 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
-
+def get_order_sum(conn, month):
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+        cursor.execute("""
+            SELECT c.customer_name, SUM(o.total_amount) AS total_amount
+            FROM orders o
+            JOIN customers c ON o.customer_id = c.customer_id
+            WHERE EXTRACT(MONTH FROM o.order_date) = %s
+            GROUP BY c.customer_name
+            ORDER BY total_amount DESC
+        """, (month,))
+        
+        return "\n".join(
+            f"Покупатель {row['customer_name']} совершил покупок на сумму {row['total_amount']}"
+            for row in cursor.fetchall()
+        )
 # END
