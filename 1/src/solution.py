4bd66fc958b4fb902525a24765bd9729cbb77b69
Максим <160880502+elow1111@users.noreply.github.com>
2024-12-06 16:53:37 +0300

    solution.py

--- 1/src/solution.py
+++ 1/src/solution.py
@@ -4,5 +4,21 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def add_movies(conn):
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO movies (title, release_year, duration) VALUES (%s, %s, %s)",
+            ('Godfather', 1972, 175)
+        )
+        cursor.execute(
+            "INSERT INTO movies (title, release_year, duration) VALUES (%s, %s, %s)",
+            ('The Green Mile', 1999, 189)
+        )
+        conn.commit()
 
+def get_all_movies(conn):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM movies")
+        movies = cursor.fetchall()
+        return [(movie[1], movie[2], movie[3]) for movie in movies]
 # END
