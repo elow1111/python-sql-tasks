8ac7ac1530592ba874d3a19de800846f47d1e6eb
Максим <160880502+elow1111@users.noreply.github.com>
2024-12-06 16:57:41 +0300

    solution.py

--- 3/src/solution.py
+++ 3/src/solution.py
@@ -5,5 +5,18 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def batch_insert(conn, products):
+    with conn.cursor() as cursor:
+        data = [(product['name'], product['price'], product['quantity']) for product in products]
+        execute_values(
+            cursor,
+            "INSERT INTO products (name, price, quantity) VALUES %s",
+            data
+        )
+        conn.commit()
 
+def get_all_products(conn):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT id, name, price, quantity FROM products ORDER BY price DESC")
+        return cursor.fetchall()
 # END
